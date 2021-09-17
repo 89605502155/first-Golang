0925b32baac286b20c833d40bf657b3fe9d3dec2
89605502155 <ferubko1999@yandex.ru>
2021-09-17 15:34:29 +0300

    I append one new page in our web-site.

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Russia North Ecology Map")
+}
+
+func article_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Articles set will be here. ")
+}
+
+func map_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Map will be here")
+}
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/articles/", article_page)
+	http.HandleFunc("/map/", map_page)
+	http.ListenAndServe(":8080", nil)
+}
+
 func main() {
-	handler.handleRequest()
+	handleRequest()
 }
